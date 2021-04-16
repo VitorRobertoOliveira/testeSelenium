5fff9823b47c09762eacb6558b9202317938c6c4
vitor.roberto <vitorrobertooliveira@gmail.com>
2021-04-16 17:21:56 -0300

    commit correcao

--- teste_Selenium/src/main/java/runner/Run_Test.java
+++ teste_Selenium/src/main/java/runner/Run_Test.java
@@ -9,6 +9,7 @@ import io.cucumber.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "classpath:features", 
 				 glue = "steps", 
+				 plugin = {"pretty","html:build/reports/cucumber","json:build/reports/cucumber-tests/test.json"},
 				 tags = "@CompleteForm")
 public class Run_Test {
 	
